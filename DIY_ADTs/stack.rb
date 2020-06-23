e14a2034da417d47330f4eec8fc92a7f106dbd6a
Joseph Jeong <jayjunkyu@gmail.com>
2020-06-23 14:29:43 -0400

    Added Stack class

---
+++ DIY_ADTs/stack.rb
@@ -0,0 +1,32 @@
+class Stack
+    def initialize
+        @stack = Array.new()
+    end
+
+    def push(el)
+        # pushes an element
+        stack << el
+    end
+
+    def pop
+        # removes top element
+        stack.pop
+    end
+
+    def peek
+        # returns top element
+        stack[-1]
+    end
+
+    private
+    attr_accessor :stack
+end
+
+if __FILE__ == $PROGRAM_NAME
+    my_stack = Stack.new()
+    my_stack.push(1)
+    my_stack.push(5)
+    my_stack.push(3)
+    my_stack.pop
+    puts my_stack.peek
+end
